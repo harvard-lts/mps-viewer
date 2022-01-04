64bd0c6bf9b8dd04113e4e029e37a9b68c81d6b2
Phil Plencner <philip_plencner@harvard.edu>
2022-01-04 14:47:15 -0500

    Viewer example now completely uses embed service.

--- routes/index.js
+++ routes/index.js
@@ -14,22 +14,36 @@ const getUserFromReq = (req) => {
 
 router.get("/", function (req, res) {
   res.render("index", {
-      title: "Docker NodeJS Template!",
-      body: "Hello, World!",
+      title: "Welcome to the MPS Viewer!",
   });
 });
 
 router.get("/example/:recordIdentifier", async function (req, res) {
+    let viewerURL = '';
+    let title = '';
+    let iiifManifest = '';
+    let errorMsg = '';
+
     embed = await embedCtrl.getEmbed(req.params.recordIdentifier);
 
-    viewerURL = embed.data.html;
-    title = embed.data.title;
-    iiifManifest = embed.data.iiif_manifest;
+    if (embed.hasOwnProperty('error')) {
+        errorMsg = embed.error;
+    }
+    if (embed.data.hasOwnProperty('html')) {
+        viewerURL = embed.data.html;
+    }
+    if (embed.data.hasOwnProperty('title')) { 
+        title = embed.data.title;
+    }
+    if (embed.data.hasOwnProperty('iiif_manifest')) {
+        iiifManifest = embed.data.iiif_manifest;
+    }
 
     res.render("example", {
         title: title,
         viewerURL: viewerURL,
         iiifManifest: iiifManifest,
+        error: errorMsg,
     });
 });
 
