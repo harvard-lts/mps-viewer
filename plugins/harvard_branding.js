b81b07583a6d834e1829040b51e8852182051fb2
mferrarini <maura_ferrarini@harvard.edu>
2022-01-31 16:11:45 -0500

    changed logo link to point to L.H

--- plugins/harvard_branding.js
+++ plugins/harvard_branding.js
@@ -5,7 +5,7 @@ class harvardBranding extends Component {
     return (
         <div class="WithPlugins(WorkspaceControlPanel)-branding-2">
             <p class="MuiTypography-root MuiTypography-body1 MuiTypography-alignCenter">
-                <a class="MuiButtonBase-root MuiIconButton-root" tabindex="0" aria-disabled="false" href="https://digitalcollections.library.harvard.edu/" target="_blank" rel="noopener">
+                <a class="MuiButtonBase-root MuiIconButton-root" tabindex="0" aria-disabled="false" href="https://library.harvard.edu/" target="_blank" rel="noopener">
                     <span class="MuiIconButton-label">
                         <img src="/images/shield_small_crimson.png" width="100%"></img>
                     </span>
