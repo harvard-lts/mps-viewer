65b938371f85daddb8f062ba1083389ea77bfeb4
Katie Amaral <kathryn_amaral@harvard.edu>
2023-04-13 22:57:27 -0400

    Add data to controller and loop in html js

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,56 @@ const embedCtrl = require('../controllers/embed.ctrl');
 
 const { body,validationResult } = require('express-validator');
 
+/**
+ *
+ * 
+<div class="col-md-12">
+<div class="list-group">
+  <a href="/example/W401849_URN-3:HUL.ARCH:2009749" class="list-group-item list-group-item-action">Harvard University Baseball Team, photograph, 1892</a>
+  <a href="/example/W401827_URN-3:HUL.ARCH:2009747" class="list-group-item list-group-item-action">Harvard-Yale Baseball Game. Holmes Field, photograph, 1885</a>
+  <a href="/example/W587091_URN-3:RAD.SCHL:11680642" class="list-group-item list-group-item-action">To roast a chicken; show #217</a>
+  <a href="/example/HUAM140429_URN-3:HUAM:INV012574P_DYNMC" class="list-group-item list-group-item-action">Untitled Drumset</a>
+  <a href="/example/990100671200203941" class="list-group-item list-group-item-action">Hot Dog In The Manger</a>
+  <a href="/example/990095204340203941" class="list-group-item list-group-item-action">Chronique du monde depuis la création, et des rois de France et d'Angleterre, jusqu'à l'an 1461: manuscript, [ca. 1461]. MS Typ 41. Houghton Library, Harvard University, Cambridge, Mass.</a>
+  <a href="/example/990098789400203941" class="list-group-item list-group-item-action">Heures de Nôtre Dame (use of Troyes and Sens) : manuscript, [ca. 1470]</a>
+</div>
+ */
 router.get("/", function (req, res) {
+
+  const examples = [
+    {
+      "href": "/example/W401849_URN-3:HUL.ARCH:2009749",
+      "text": "Harvard University Baseball Team, photograph, 1892"
+    },
+    {
+      "href": "/example/W401827_URN-3:HUL.ARCH:2009747",
+      "text": "Harvard-Yale Baseball Game. Holmes Field, photograph, 1885"
+    },
+    {
+      "href": "/example/W587091_URN-3:RAD.SCHL:11680642",
+      "text": "To roast a chicken; show #217"
+    },
+    {
+      "href": "/example/HUAM140429_URN-3:HUAM:INV012574P_DYNMC",
+      "text": "Untitled Drumset"
+    },
+    {
+      "href": "/example/990100671200203941",
+      "text": "Hot Dog In The Manger"
+    },
+    {
+      "href": "/example/990095204340203941",
+      "text": "Chronique du monde depuis la création, et des rois de France et d'Angleterre, jusqu'à l'an 1461: manuscript, [ca. 1461]. MS Typ 41. Houghton Library, Harvard University, Cambridge, Mass."
+    },
+    {
+      "href": "/example/990098789400203941",
+      "text": "Heures de Nôtre Dame (use of Troyes and Sens) : manuscript, [ca. 1470]"
+    }
+  ];
+
   res.render("index", {
       title: "Welcome to the MPS Viewer!",
+      examples: examples
   });
 });
 
