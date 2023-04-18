0ac7e6be8aed101d1e62da22311a1fdc6f955fa4
Katie Amaral <kathryn_amaral@harvard.edu>
2023-04-18 16:34:10 -0400

    Update tests

--- __tests__/integration/integration.test.js
+++ __tests__/integration/integration.test.js
@@ -4,26 +4,26 @@ beforeAll( async () => {
   console.log('Running integration tests.');
 });
 
- describe('Test MPS Embed Healthcheck', () => {
-    test('Successful response from healthcheck route', async () => {
+describe('Test MPS Embed Healthcheck', () => {
+  test('Successful response from healthcheck route', async () => {
 
-      const embedHealthcheckRoute = process.env.EMBED_BASE_URL + '/healthcheck';
+    const embedHealthcheckRoute = process.env.EMBED_BASE_URL + '/healthcheck';
 
-      const requestOptionsData = {
-        method: 'GET',
-        url: embedHealthcheckRoute
-      };
+    const requestOptionsData = {
+      method: 'GET',
+      url: embedHealthcheckRoute
+    };
 
-      let response;
-      try {
-        response = await httpCtrl.makeRequest(requestOptionsData);
-      } catch (e) {
-        console.log(e);
-      }
+    let response;
+    try {
+      response = await httpCtrl.makeRequest(requestOptionsData);
+    } catch (e) {
+      console.log(e);
+    }
 
-      expect(response.status).toBe(200);
-      expect(response.data.hasOwnProperty('message'));
-      expect(response.data['message']).toBeDefined();
-      expect(response.data['message']).not.toBeNull();
-    });
+    expect(response.status).toBe(200);
+    expect(response.data.hasOwnProperty('message'));
+    expect(response.data['message']).toBeDefined();
+    expect(response.data['message']).not.toBeNull();
+  });
 });
