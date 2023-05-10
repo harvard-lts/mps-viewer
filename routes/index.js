d192e529d79b03075a2617b69aef5225ac312c27
Phil Plencner <philip_plencner@harvard.edu>
2023-05-10 16:19:28 -0400

    Moving examples to a separate configuration file.

--- routes/index.js
+++ routes/index.js
@@ -2,72 +2,12 @@ const express = require('express');
 const router = express.Router();
 const embedCtrl = require('../controllers/embed.ctrl');
 const consoleLogger = require('../logger/logger.js').console;
+const exampleItems = require('../config/example-items.json');
 
 router.get("/", async (req, res) => {
 
-  const idsExamples = [
-    {
-      "href": "/example/legacy/W401849_URN-3:HUL.ARCH:2009749",
-      "text": "Harvard University Baseball Team, photograph, 1892"
-    },
-    {
-      "href": "/example/legacy/W401827_URN-3:HUL.ARCH:2009747",
-      "text": "Harvard-Yale Baseball Game. Holmes Field, photograph, 1885"
-    },
-    {
-      "href": "/example/legacy/W587091_URN-3:RAD.SCHL:11680642",
-      "text": "To roast a chicken; show #217"
-    },
-    {
-      "href": "/example/legacy/HUAM140429_URN-3:HUAM:INV012574P_DYNMC",
-      "text": "Untitled Drumset"
-    },
-    {
-      "href": "/example/legacy/990100671200203941",
-      "text": "Hot Dog In The Manger"
-    },
-    {
-      "href": "/example/legacy/990095204340203941",
-      "text": "Chronique du monde depuis la création, et des rois de France et d'Angleterre, jusqu'à l'an 1461: manuscript, [ca. 1461]. MS Typ 41. Houghton Library, Harvard University, Cambridge, Mass."
-    },
-    {
-      "href": "/example/legacy/990098789400203941",
-      "text": "Heures de Nôtre Dame (use of Troyes and Sens) : manuscript, [ca. 1470]"
-    }
-  ];
-
-  const mpsExamples = [
-    {
-      "href": "/example/mps/URN-3:DIV.LIB.USC:3200357?manifestVersion=2",
-      "text": "Harvard Divinity School Unitarian Service Committee",
-      "version": 2
-    },
-    {
-      "href": "/example/mps/URN-3:DIV.LIB.USC:3200357?manifestVersion=3",
-      "text": "Harvard Divinity School Unitarian Service Committee",
-      "version": 3
-    },
-    {
-      "href": "/example/mps/URN-3:FHCL:42632611?manifestVersion=2",
-      "text": "Harvard Yenching Fushun Xian zhi 37 juan",
-      "version": 2
-    },
-    {
-      "href": "/example/mps/URN-3:FHCL:42632611?manifestVersion=3",
-      "text": "Harvard Yenching Fushun Xian zhi 37 juan",
-      "version": 3
-    },
-    {
-      "href": "/example/mps/URN-3:FHCL:100001249?manifestVersion=2",
-      "text": "Tibetan Buddhist Resource Center",
-      "version": 2
-    },
-    {
-      "href": "/example/mps/URN-3:FHCL:100001249?manifestVersion=2",
-      "text": "Tibetan Buddhist Resource Center",
-      "version": 3
-    }
-  ];
+  const idsExamples = exampleItems.idsExamples;
+  const mpsExamples = exampleItems.mpsExamples;
 
   res.render("index", {
     title: "Welcome to the MPS Viewer!",
