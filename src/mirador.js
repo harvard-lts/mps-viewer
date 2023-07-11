7e368540b1f23adfd9e0d7c613cd845d91c2d91a
mferrarini <maura_ferrarini@harvard.edu>
2023-07-11 16:36:48 -0400

    include gtm container id and add analytics plugin to documentation

--- src/mirador.js
+++ src/mirador.js
@@ -345,8 +345,7 @@ const config = {
     mitmPath: `${new URL(window?.frameElement?.src ?? window.origin).origin}/pdiiif/mitm.html`,
   },
   miradorAnalyticsPlugin: {
-    scriptTag: "<!-- Google Tag Manager --><script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-WSHSDQ5');</script><!-- End Google Tag Manager -->",
-    noscriptTag: '<!-- Google Tag Manager (noscript) --><noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WSHSDQ5"height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript><!-- End Google Tag Manager (noscript) -->',
+    containerID:'GTM-WSHSDQ5',
   },
 };
 
@@ -355,12 +354,12 @@ const plugins = [
   ...miradorPdiiifPlugin,
   ...miradorSharePlugin,
   ...miradorImageToolsPlugin,
+  ...miradorAnalyticsPlugin,
   miradorUrlSyncPlugin,
   harvardBranding,
   harvardHelp,
   hideViewerNavigation,
   copyrightLabel,
-  miradorAnalyticsPlugin,
 ]
 
 Mirador.viewer(config, plugins);
