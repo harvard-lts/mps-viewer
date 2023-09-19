61ec386b3998377c3885a60ad4c9ffd627fdd5c6
Phil Plencner <philip_plencner@harvard.edu>
2023-09-19 12:37:51 -0400

    Adding stub for Successful response from getEmbed Legacy.

--- __tests__/unit/embed.test.js
+++ __tests__/unit/embed.test.js
@@ -1,3 +1,4 @@
+const sinon = require('sinon');
 const embedCtrl = require('../../controllers/embed.ctrl');
 const httpCtrl = require('../../controllers/http.ctrl');
 const consoleLogger = require('../../logger/logger.js').console;
@@ -11,6 +12,7 @@ describe('Embed', () => {
   test('Successful response from getEmbed Legacy', async () => {
     const recordIdentifier = 'HUAM140429_URN-3:HUAM:INV012574P_DYNMC';
     let embed;
+    let stub = sinon.stub(httpCtrl, 'makeRequest').returns({"data":[{"title":"Harvard University Baseball Team, photograph, 1892","iiifManifest":"https://iiif.lib.harvard.edu/manifests/ids:10274486","html":"<iframe src='https://localhost:23017/viewer/?manifestId=https%3A%2F%2Fiiif.lib.harvard.edu%2Fmanifests%2Fids%3A10274486' height='700px' width='1200px' title='[Harvard University Baseball Team, photograph, 1892]' frameborder='0' marginwidth='0' marginheight='0' scrolling='no' allowfullscreen></iframe>"}]});
     try {
       embed = await embedCtrl.getEmbed(recordIdentifier, 'legacy', 2);
     } catch (e) {
