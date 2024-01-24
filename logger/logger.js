75657f8fbc65cca23bbc02b93e70e5e2aa05d3b7
Phil Plencner <philip_plencner@harvard.edu>
2024-01-24 13:58:54 -0500

    Adding ability to turn off file logging.

--- logger/logger.js
+++ logger/logger.js
@@ -4,14 +4,26 @@ const os = require('os');
 const hostname = os.hostname();
 const rfs = require('rotating-file-stream');
 require('winston-daily-rotate-file');
+const dateFns = require('date-fns');
 
 const logger = {};
 
-// Console logs and errors write to file using winston
-logger.console = winston.createLogger({
-    level: 'debug',
-    format: winston.format.json(),
-    transports: [
+logger.logLevel = process.env.APP_LOG_LEVEL || 'debug';
+
+logger.dateFormatStr = 'yyyy-MM-dd';
+
+logger.dateNow = dateFns.format(new Date(), logger.dateFormatStr);
+
+logger.skipRoutes = [
+  "/version",
+  "/healthcheck",
+  "/robots.txt",
+  "/favicon.ico"
+];
+
+let consoleTransports = '';
+if (process.env.LOG_TO_STDOUT === 'false') {
+    consoleTransports = [
         new winston.transports.DailyRotateFile(
             {
                 filename: `logs/${hostname}/console/%DATE%-console.log`,
@@ -21,13 +33,37 @@ logger.console = winston.createLogger({
             }),
         new winston.transports.Console({ format: winston.format.simple() })
     ]
+}
+else {
+    consoleTransports = [
+        new winston.transports.Console({ format: winston.format.simple() })
+    ];
+}
+
+logger.skipLogs = (req, res) => {
+  let skipLogsAnswer = false;
+  logger.skipRoutes.forEach(routeToSkip => {
+    if (req.originalUrl && req.originalUrl.indexOf(routeToSkip) > -1) {
+      skipLogsAnswer = true;
+    }
+  });
+  return skipLogsAnswer;
+}
+
+// Console logs and errors write to file using winston
+logger.console = winston.createLogger({
+  level: logger.logLevel,
+  format: winston.format.json(),
+  transports: consoleTransports
 });
 
-// API request logs write to file config
-logger.requestLogStream = rfs.createStream('request.log', {
+if (process.env.LOG_TO_STDOUT === 'false') {
+    // API request logs write to file config
+    logger.requestLogStream = rfs.createStream(`./${hostname}_request_${logger.dateNow}.log`, {
     interval: '1d', // Rotate daily
     maxFiles: 30, // Maximum number of rotated files to keep in storage
-    path: path.join(__dirname, `../logs/${hostname}/request/`)
-});
+    path: path.join(__dirname, `../logs`)
+    });
+}
 
 module.exports = logger;
