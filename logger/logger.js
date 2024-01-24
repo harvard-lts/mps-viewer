b393fdc7cbd4186f069de232a41a1271c1b13760
Phil Plencner <philip_plencner@harvard.edu>
2024-01-24 15:12:54 -0500

    Updating logging to match node-ci-template.

--- logger/logger.js
+++ logger/logger.js
@@ -1,4 +1,5 @@
-const winston = require('winston');
+const { createLogger, format, transports } = require('winston');
+const { combine, timestamp, label, printf } = format;
 const path = require('path');
 const os = require('os');
 const hostname = os.hostname();
@@ -24,19 +25,20 @@ logger.skipRoutes = [
 let consoleTransports = '';
 if (process.env.LOG_TO_STDOUT === 'false') {
     consoleTransports = [
-        new winston.transports.DailyRotateFile(
-            {
-                filename: `logs/${hostname}/console/%DATE%-console.log`,
-                datePattern: 'yyyy-MM-DD',
-                maxSize: '20m',
-                maxFiles: '14d'
-            }),
-        new winston.transports.Console({ format: winston.format.simple() })
+      new transports.DailyRotateFile(
+        {
+          filename: `${hostname}_console_%DATE%.log`,
+          dirname: `logs`,
+          dateFormat: logger.dateFormatStr,
+          maxSize: '20m',
+          maxFiles: '14d'
+        }),
+      new transports.Console({ format: format.simple() })
     ]
 }
 else {
     consoleTransports = [
-        new winston.transports.Console({ format: winston.format.simple() })
+      new transports.Console({ format: format.simple() })
     ];
 }
 
@@ -50,20 +52,31 @@ logger.skipLogs = (req, res) => {
   return skipLogsAnswer;
 }
 
+const loggerFormat = printf(info => {
+  if(info instanceof Error) {
+      return `${info.timestamp} ${info.level}: ${info.message} ${info.stack}`;
+  }
+  return `${info.timestamp} ${info.level}: ${info.message}`;
+});
+
 // Console logs and errors write to file using winston
-logger.console = winston.createLogger({
+logger.console = createLogger({
   level: logger.logLevel,
-  format: winston.format.json(),
+  format: combine(
+    format.splat(),
+    timestamp(),
+    loggerFormat
+  ),
   transports: consoleTransports
 });
 
 if (process.env.LOG_TO_STDOUT === 'false') {
-    // API request logs write to file config
-    logger.requestLogStream = rfs.createStream(`./${hostname}_request_${logger.dateNow}.log`, {
+  // API request logs write to file config
+  logger.requestLogStream = rfs.createStream(`./${hostname}_request_${logger.dateNow}.log`, {
     interval: '1d', // Rotate daily
     maxFiles: 30, // Maximum number of rotated files to keep in storage
     path: path.join(__dirname, `../logs`)
-    });
+  });
 }
 
 module.exports = logger;
